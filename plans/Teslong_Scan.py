b8c7165029ec446c34fc7e3148be2bd25bc1b037
Mrinal Bera <nayanbera@gmail.com>
2025-05-07 16:28:24 -0500

    fix the problems of cross-hair in sample_view.py

--- plans/Teslong_Scan.py
+++ plans/Teslong_Scan.py
@@ -1,14 +1,18 @@
+## Teslong Image Capture Scripts
+# Autofocuse funtion
+# Syringe pump control functions
+# Image capture and save into HDF functions
+# ver 1.0: fixed live table display in the queue server when running capture image function
+# !!! Saving path need to be setup first
+
 from ophyd import EpicsSignal, EpicsMotor
 import pandas as pd
 from ophyd.signal import EpicsSignal
-from bluesky.callbacks import LiveFit, LiveFitPlot, LivePlot, LiveTable
-from bluesky.plan_stubs import mv, mvr, sleep, abs_set
-from bluesky import RunEngine
+from bluesky.preprocessors import run_decorator
+from bluesky.plan_stubs import mv, mvr, sleep,trigger_and_read
 import epics
 import numpy as np
-import matplotlib.pyplot as plt
 import cv2
-import time
 import os
 
 ## Initiate Motor control
@@ -25,6 +29,7 @@ phdUltra_pump_targetvolume = EpicsSignal('15IDD:PHDUltra:TargetVolume', string=T
 Teslong_file_path = EpicsSignal('Teslong:HDF1:FilePath', string=True)
 Teslong_file_number = EpicsSignal('Teslong:HDF1:FileNumber')
 Teslong_file_name = EpicsSignal('Teslong:HDF1:FileName', string=True)
+Teslong_full_file_name = EpicsSignal('Teslong:HDF1:FullFileName_RBV', string=True)
 Teslong_file_template = EpicsSignal('Teslong:HDF1:FileTemplate', string=True)
 Teslong_enable_callbacks = EpicsSignal('Teslong:HDF1:EnableCallbacks')
 Teslong_write_file = EpicsSignal('Teslong:HDF1:WriteFile')
@@ -51,7 +56,6 @@ Teslong_auto_increment.put(0)
 
 def set_infuse_flowrate_phdUltra(flow_rate):
     """
-
     :param flow_rate: flow rate in uL/min
     """
     # yield from mv(phdUltra_pump_flowrate, flow_rate)
@@ -128,6 +132,43 @@ def autofocus(delta_z):
     # print('Focus Done')
 
 
+def capture_live_image(position_csv_path,
+        file_prefix,
+        interval,
+        max_i,
+        x_offset=0,
+        y_offset=0,
+        position_test=False,
+        md ={}):
+    md = md or {}
+    _md = {'plan_args': {'position_csv_path': position_csv_path,
+                         'file_prefix': file_prefix,
+                         'interval': interval,
+                         'max_i': max_i,
+                         'x_offset':x_offset,
+                         'y_offset': y_offset,
+                         'position_test':position_test},
+
+            'plan_name': 'capture_live_image',
+            'expDir':'/cars6/Data/chemdata/ASWAXS/Software/ASWAXS/Data'
+           }
+    _md.update(md)
+    @run_decorator(md = _md)
+    def inner_function():
+         yield from capture_images_from_list(
+            position_csv_path,
+            file_prefix,
+            interval,
+            max_i,
+            x_offset=x_offset,
+            y_offset=y_offset,
+            position_test=position_test,
+            md=md)
+    return (yield from inner_function())
+
+
+
+
 def capture_images_from_list(
         position_csv_path,
         file_prefix,
@@ -135,7 +176,8 @@ def capture_images_from_list(
         max_i,
         x_offset=0,
         y_offset=0,
-        position_test=False):
+        position_test=False,
+        md = {}):
     """
     Perform Teslong scan based on a preset position path in a csv file.
 
@@ -159,35 +201,41 @@ def capture_images_from_list(
 
     """
     # Set offset for motors
-    print(f"x_offset set to {x_offset}, y_offset set to {y_offset}")
+    # print(f"x_offset set to {x_offset}, y_offset set to {y_offset}")
 
     # Setup file saving
-    file_path_to_use = os.path.join('/chemdata/Data/ASWAXS/Jiajun/Data/', file_prefix)
+    # file_path_to_save = os.path.join(md['expDir'], file_prefix)
+    file_path_to_save = md['expDir']
+    # os.makedirs(file_path_to_save, exist_ok=True)
+    # print('path created!!!')
+
+    Teslong_file_path.put(file_path_to_save)
 
     if not Teslong_HDF_path_exist.get():
-        print('HDF path does not exist')
+        print(f'HDF path {file_path_to_save}'
+              f' does not exist')
         return -1
-
-    os.makedirs(file_path_to_use, exist_ok=True)
-
-    Teslong_file_path.put(file_path_to_use)
 
     """Read x, y, z positions from an Excel file and capture images at those positions with a specified interval."""
     df = pd.read_csv(position_csv_path)  # Adjusted to read Excel files
 
     # Set file prefix for naming
     Teslong_file_name.put(file_prefix)
-    # Teslong_image_mode.put(1)  # Set to multiple mode
 
-    # Assuming the columns are named 'x', 'y', 'z'
     x_values, y_values = df['#Vertex_X'].values, df['Vertex_Y'].values  # Adjusted column access
 
     if position_test:
+        # Capture images at intervals specified by 'interval'
+        if max_i is None:
+            stop_idx = len(x_values)
+        else:
+            stop_idx = max_i
+        scan_num = 0
         Teslong_enable_callbacks.put(0)  # Enable callbacks for data capture
         Teslong_image_mode.put(2)
         Teslong_acquire_data.put(1)  # Start acquiring data
         scan_num = 0
-        for i in range(0, len(x_values), interval):  # Iterate with the given interval
+        for i in range(0, stop_idx, interval):  # Iterate with the given interval
             tx, ty = x_values[i], y_values[i]
             yield from mv(sx, tx + x_offset, sy, ty + y_offset)  # Move motors to (x, y)
             yield from sleep(2)  # Wait for motors to settle
@@ -195,7 +243,8 @@ def capture_images_from_list(
             # Teslong_file_number.put(i)  # Update file number for saving
             yield from autofocus(0.5)  # Adjust autofocus
 
-            print(f"Scan {scan_num}: Test at ({tx}, {ty}).")  # Log position
+            yield from trigger_and_read([sx,sy,cmir,Teslong_full_file_name])
+            # print(f"Scan {scan_num}: Test at ({tx}, {ty}).")  # Log position
             scan_num +=1
     else:
         # Capture images at intervals specified by 'interval'
@@ -204,6 +253,7 @@ def capture_images_from_list(
         else:
             stop_idx = max_i
         scan_num = 0
+
         for i in range(0, stop_idx, interval):  # Iterate with the given interval
             tx, ty = x_values[i], y_values[i]
             yield from mv(sx, tx + x_offset, sy, ty + y_offset)  # Move motors to (x, y)
@@ -213,9 +263,8 @@ def capture_images_from_list(
             yield from autofocus(0.5)  # Adjust autofocus
 
             Teslong_enable_callbacks.put(1)  # Enable callbacks for data capture
-            # Teslong_acquire_data.put(1)  # Start acquiring data
-            # yield from sleep(3)  # Wait for data acquisition
-            print(f"Scan {scan_num}: Image  captured at ({tx}, {ty}) and saved as {file_prefix}_{i:03d}.")  # Log position
+            yield from trigger_and_read([sx, sy, cmir, Teslong_full_file_name])
+            # print(f"Scan {scan_num}: Image  captured at ({tx}, {ty}) and saved as {file_prefix}_{i:03d}.")  # Log position
             scan_num += 1
     Teslong_enable_callbacks.put(0)  # Turn off callbacks after scans
 
